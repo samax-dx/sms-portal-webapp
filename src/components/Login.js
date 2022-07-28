d89cced01de4f129d141125b487872c5f32f6a7c
NHSanto <nahid170053@diit.edu.bd>
2022-07-28 20:23:06 +0600

    Title added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useActor } from "@xstate/react";
-import { Button, Form, Input, Alert, Row, Card, Divider } from "antd";
+import { Button, Form, Input, Alert, Row, Card, Divider,Descriptions } from "antd";
 import getAllConfig from '../config/main'
 
 export const Login = ({ actor }) => {
@@ -18,8 +18,9 @@ export const Login = ({ actor }) => {
     return (
         <Row type="flex" justify="center" align="middle" style={{ minHeight: '100vh' }}>
             <Card style={{width: "300px"}}>
+                <Divider style={{margin: 0, fontSize:25}}>SMS Client Portal</Divider>
                 <img height={120} src={getAllConfig.logo} style={{display: "block",margin: "0 auto"}}/>
-                <Divider style={{marginTop: 0}}>Client Login</Divider>
+                <Divider style={{marginTop: 0}} >Client Login</Divider>
                 <Form form={loginForm} size="large">
                     <Form.Item name="loginId">
                         <Input placeholder="User ID"/>
